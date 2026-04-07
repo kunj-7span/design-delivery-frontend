a71b1c16b50654816376c881ba0e9e48e6987818
kunj-7span <kunj@7span.com>
2026-04-07 19:35:16 +0530

    [Add]: edit button and remove button in client invitation

--- src/components/auth/register-page-form.jsx
+++ src/components/auth/register-page-form.jsx
@@ -69,7 +69,7 @@ const RegisterPageForm = () => {
                 contactPersonName: "",
               });
             }
-          }else{
+          } else {
             console.log(response)
           }
         }
@@ -77,7 +77,7 @@ const RegisterPageForm = () => {
         console.error("Error validating invitation token:", err);
         setError("root", {
           type: "manual",
-          message: "Invalid or expired invitation link. Please try again.",
+          message: "Invalid or expired invitation link.",
         });
       } finally {
         setIsLoading(false);
@@ -197,7 +197,7 @@ const RegisterPageForm = () => {
         role: data.role,
         ...(avatarUrl && { avatar: avatarUrl }),
       };
-      console.log("123456",payload)
+      console.log("123456", payload)
 
       await axios.post(
         `${import.meta.env.VITE_API_BASE_URL}/auth/register`,
@@ -226,9 +226,9 @@ const RegisterPageForm = () => {
       }
 
       // Navigate to client dashboard if registered via invitation
-      
-        navigate("/verify-otp");
-      
+
+      navigate("/verify-otp");
+
     } catch (err) {
       setError("root", {
         type: "server",
@@ -269,135 +269,134 @@ const RegisterPageForm = () => {
             </Button>
           </div>
 
-      {/* Profile picture */}
-      <div className="mb-4 flex flex-col items-center gap-2">
-        <input
-          type="file"
-          ref={fileInputRef}
-          className="hidden"
-          accept=".png, .jpg, .jpeg, image/png, image/jpeg"
-          onChange={handleFileChange}
-        />
-        <div className="relative inline-block">
-          <div
-            onClick={!profilePicPreview ? handleFileClick : undefined}
-            className={`w-18 h-18 rounded-full border-2 flex items-center justify-center overflow-hidden ${
-              profilePicPreview
-                ? "border-0"
-                : "border-dashed border-primary hover:border-hover-primary hover:bg-purple-100 cursor-pointer"
-            } transition relative group bg-purple-50`}
-          >
-            {profilePicPreview ? (
-              <img
-                src={profilePicPreview}
-                alt="Profile"
-                className="w-full h-full object-cover"
-              />
-            ) : role === "agency_admin" ? (
-              <Building2
-                className="text-primary group-hover:opacity-0"
-                size={24}
-              />
-            ) : (
-              <User className="text-primary group-hover:opacity-0" size={24} />
-            )}
-            <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 flex items-center justify-center transition">
-              <Camera className="text-white" size={20} />
-            </div>
-          </div>
+          {/* Profile picture */}
+          <div className="mb-4 flex flex-col items-center gap-2">
+            <input
+              type="file"
+              ref={fileInputRef}
+              className="hidden"
+              accept=".png, .jpg, .jpeg, image/png, image/jpeg"
+              onChange={handleFileChange}
+            />
+            <div className="relative inline-block">
+              <div
+                onClick={!profilePicPreview ? handleFileClick : undefined}
+                className={`w-18 h-18 rounded-full border-2 flex items-center justify-center overflow-hidden ${profilePicPreview
+                    ? "border-0"
+                    : "border-dashed border-primary hover:border-hover-primary hover:bg-purple-100 cursor-pointer"
+                  } transition relative group bg-purple-50`}
+              >
+                {profilePicPreview ? (
+                  <img
+                    src={profilePicPreview}
+                    alt="Profile"
+                    className="w-full h-full object-cover"
+                  />
+                ) : role === "agency_admin" ? (
+                  <Building2
+                    className="text-primary group-hover:opacity-0"
+                    size={24}
+                  />
+                ) : (
+                  <User className="text-primary group-hover:opacity-0" size={24} />
+                )}
+                <div className="absolute inset-0 bg-black/20 opacity-0 group-hover:opacity-100 flex items-center justify-center transition">
+                  <Camera className="text-white" size={20} />
+                </div>
+              </div>
 
-          {profilePicPreview && (
+              {profilePicPreview && (
+                <button
+                  type="button"
+                  onClick={handleRemoveProfilePic}
+                  className="cursor-pointer absolute -top-2 -right-4 bg-red-500 hover:bg-red-600 rounded-full p-1 text-white transition shadow-lg"
+                >
+                  <X size={16} />
+                </button>
+              )}
+            </div>
+
             <button
               type="button"
-              onClick={handleRemoveProfilePic}
-              className="cursor-pointer absolute -top-2 -right-4 bg-red-500 hover:bg-red-600 rounded-full p-1 text-white transition shadow-lg"
+              onClick={handleFileClick}
+              className="text-xs text-primary hover:text-hover-primary font-medium"
             >
-              <X size={16} />
+              Set profile picture
             </button>
-          )}
-        </div>
-
-        <button
-          type="button"
-          onClick={handleFileClick}
-          className="text-xs text-primary hover:text-hover-primary font-medium"
-        >
-          Set profile picture
-        </button>
-      </div>
-
-      <div className="flex flex-col gap-3">
-        {errors.root && (
-          <div className="p-3 bg-red-100 text-red-600 rounded-xl text-sm text-center">
-            {errors.root.message}
           </div>
-        )}
 
-        <InputField
-          label={role === "agency_admin" ? "Agency Name" : "Client Name"}
-          id="name"
-          type="text"
-          icon={role === "agency_admin" ? Building2 : User}
-          placeholder={role === "agency_admin" ? "Agency name" : "Client name"}
-          autoComplete="organization"
-          error={errors.name}
-          {...register("name")}
-        />
+          <div className="flex flex-col gap-3">
+            {errors.root && (
+              <div className="p-3 bg-red-100 text-red-600 rounded-xl text-sm text-center">
+                {errors.root.message}
+              </div>
+            )}
 
-        <InputField
-          label="Email"
-          id="email"
-          type="email"
-          icon={Mail}
-          placeholder={
-            role === "agency_admin" ? "Agency email" : "Client email"
-          }
-          autoComplete="email"
-          error={errors.email}
-          {...register("email")}
-        />
+            <InputField
+              label={role === "agency_admin" ? "Agency Name" : "Client Name"}
+              id="name"
+              type="text"
+              icon={role === "agency_admin" ? Building2 : User}
+              placeholder={role === "agency_admin" ? "Agency name" : "Client name"}
+              autoComplete="organization"
+              error={errors.name}
+              {...register("name")}
+            />
 
-        <InputField
-          label="Password"
-          id="password"
-          type="password"
-          icon={Lock}
-          placeholder="Enter password"
-          autoComplete="new-password"
-          error={errors.password}
-          {...register("password")}
-        />
+            <InputField
+              label="Email"
+              id="email"
+              type="email"
+              icon={Mail}
+              placeholder={
+                role === "agency_admin" ? "Agency email" : "Client email"
+              }
+              autoComplete="email"
+              error={errors.email}
+              {...register("email")}
+            />
 
-        <InputField
-          label="Confirm Password"
-          id="confirmPassword"
-          type="password"
-          icon={Lock}
-          placeholder="Confirm new password"
-          autoComplete="new-password"
-          error={errors.confirmPassword}
-          {...register("confirmPassword")}
-        />
+            <InputField
+              label="Password"
+              id="password"
+              type="password"
+              icon={Lock}
+              placeholder="Enter password"
+              autoComplete="new-password"
+              error={errors.password}
+              {...register("password")}
+            />
 
-        <div className="mt-2">
-          <Button
-            type="submit"
-            isLoading={isSubmitting}
-            className="px-4 py-4 bg-primary hover:bg-hover-primary shadow-md shadow-indigo-200 text-white text-sm w-full btn-class"
-          >
-          {role === "agency_admin"
-              ? "Create Agency Account"
-              : "Create Client Account"}
-          </Button>
+            <InputField
+              label="Confirm Password"
+              id="confirmPassword"
+              type="password"
+              icon={Lock}
+              placeholder="Confirm new password"
+              autoComplete="new-password"
+              error={errors.confirmPassword}
+              {...register("confirmPassword")}
+            />
 
-          <div className="text-xs mt-5 text-center font-semibold flex items-center justify-center gap-1">
-            <span>Already have an account?</span>
-            <Link to="/login" className="text-primary hover:text-hover-primary">
-              Log in
-            </Link>
+            <div className="mt-2">
+              <Button
+                type="submit"
+                isLoading={isSubmitting}
+                className="px-4 py-4 bg-primary hover:bg-hover-primary shadow-md shadow-indigo-200 text-white text-sm w-full btn-class"
+              >
+                {role === "agency_admin"
+                  ? "Create Agency Account"
+                  : "Create Client Account"}
+              </Button>
+
+              <div className="text-xs mt-5 text-center font-semibold flex items-center justify-center gap-1">
+                <span>Already have an account?</span>
+                <Link to="/login" className="text-primary hover:text-hover-primary">
+                  Log in
+                </Link>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
         </form>
       )}
     </>
